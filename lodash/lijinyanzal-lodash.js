ed34e9c39659c8c715f1d692675e07d8323b9fa4
lijinyan89 <2979096145@qq.com>
2019-01-15 19:34:15 +0800

    add compact,rewrite chunk

--- lodash/lijinyanzal-lodash.js
+++ lodash/lijinyanzal-lodash.js
@@ -2,20 +2,30 @@
 var lijinyanzal = function(){
 
   function chunk(array, size = 1) {
-    let count = 0
-    let ary = []
-    array.forEach(item => {
-      if (count % size == 0) {
-        ary[Math.floor(count / size)] = []
-      }
-      ary[Math.floor(count / size)].push(item)
+    let times = Math.ceil(array.length / size)
+    let item = []
+    let result = []
+    count = 0
+    for (let i = 0; i < array.length; i++) {
+      item.push(array[i])
       count++
-    })
-    return ary
+      if (count % size == 0 || i == array.length - 1) {
+        result.push(item)
+        item = []
+      }
+    }
+    return result
   }
-
-
+  
+  function compact(ary) {
+    return ary.filter(it =>
+      it)
+  }
+  
+  
+  
   return {
-    chunk: chunk,
+    chunk,
+    compact,
   }
 }()
